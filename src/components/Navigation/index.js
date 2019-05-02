2c0c1e6aaed0f290be7dd5537babd6892b5e9be2
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2019-05-02 15:52:46 +0200

    customize navigation based on authenticated user

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
     <div>
+        {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
+    </div>
+);
+
+const NavigationAuth = () => (
         <ul>
             <li>
                 <Link to={ROUTES.SIGN_IN}>Sign In</Link>
@@ -26,6 +31,16 @@ const Navigation = () => (
                 <SignOutButton />
             </li>
         </ul>
-    </div>
+);
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+            <Link to={ROUTES.SIGN_IN}>Sing In</Link>
+        </li>
+    </ul>
 );
 export default Navigation;
